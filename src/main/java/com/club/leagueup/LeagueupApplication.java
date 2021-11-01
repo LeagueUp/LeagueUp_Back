ef3fd73f4b1255826d09c9dd103bb7f4c40d93b3
yulhee741 <dbfgml980413@gmail.com>
2021-11-02 02:01:14 +0900

    init

---
+++ src/main/java/com/club/leagueup/LeagueupApplication.java
@@ -0,0 +1,13 @@
+package com.club.leagueup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/club/leagueup/LeagueupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.club.leagueup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
